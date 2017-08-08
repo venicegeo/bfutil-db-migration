5c3f848fd3dca2f5fbcee067f5b14b26d6daaf2d
Patrick D <patrick.doody@rgi-corp.com>
2017-08-08 08:01:45 -0400

    Update Namespace

--- src/main/java/org/venice/piazza/Migration.java
+++ src/main/java/org/venice/piazza/Migration.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.venice.piazza.common.hibernate.dao.DataResourceDao;
+import org.venice.piazza.common.hibernate.dao.dataresource.DataResourceDao;
 import org.venice.piazza.common.hibernate.entity.DataResourceEntity;
 
 import model.data.DataResource;
